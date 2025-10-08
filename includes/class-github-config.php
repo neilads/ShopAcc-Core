48c2b45dfff2af2558346aaeda4f9ac2abaf764a
neilads <phamthien.ads@icloud.com>
2025-10-09 00:42:47 +0700

    Update

--- includes/class-github-config.php
+++ includes/class-github-config.php
@@ -7,7 +7,7 @@ if (!defined('ABSPATH')) {
 class ShopAcc_GitHub_Config {
     
     public static function get_github_username() {
-        return apply_filters('shopacc_github_username', 'your-github-username');
+        return apply_filters('shopacc_github_username', 'neilads');
     }
     
     public static function get_github_repo() {
@@ -15,10 +15,10 @@ class ShopAcc_GitHub_Config {
     }
     
     public static function get_github_token() {
-        return apply_filters('shopacc_github_token', '');
+        return apply_filters('shopacc_github_token', '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');
     }
     
     public static function is_private_repo() {
-        return apply_filters('shopacc_github_private', false);
+        return apply_filters('shopacc_github_private', true);
     }
 }
